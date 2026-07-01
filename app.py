da15079a2fdecb809a5ebf50dbe0334da53f0f3b
Ahsan Rahul <rahul.ahsan10@gmail.com>
2026-07-01 19:20:17 +0000

    stable version of the website, still working on adding bootstrap CSS and JS into it

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ import os
 from dotenv import load_dotenv
 import git
 from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm
+from flask_behind_proxy import FlaskBehindProxy
 
 load_dotenv()
 FLASK_SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
+
+app = Flask(__name__)                    # this gets the name of the file so Flask knows it's name
+proxied = FlaskBehindProxy(app)
+app.config['SECRET_KEY'] = FLASK_SECRET_KEY
+app.debug = True
+# toolbar = DebugToolbarExtension(app)              #yeah this doesnt work 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
@@ -24,12 +32,6 @@ with app.app_context():
   db.create_all()
 
 
-app = Flask(__name__)                    # this gets the name of the file so Flask knows it's name
-proxied = FlaskBehindProxy(app)
-app.config['SECRET_KEY'] = FLASK_SECRET_KEY
-app.debug = True
-# toolbar = DebugToolbarExtension(app)              #yeah this doesnt work 
-
 @app.route("/")                          # this tells you the URL the method below is related to
 @app.route("/home")
 def home():
@@ -38,14 +40,20 @@ def home():
 # def hello_world():
 #     return "<p>Hello, World But this time with a webhook again again</p>"        # this prints HTML to the webpage
 
+@app.route("/second_page")
+def second_page():
+    return render_template('second_page.html', subtitle='Second Page', text='This is the second page')
+
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit(): # checks if entries are valid
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
-
 
 @app.route("/update_server", methods=['POST'])
 def webhook():
